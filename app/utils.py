30b70a5219b62c8c12c4133f9ec7ea9274225b50
Mauricio Pizarro <pizarromauricio031096@gmail.com>
2026-01-15 01:35:37 -0300

    Feat: implement redis, docker compose and histogram endpoint (#3)

--- app/utils.py
+++ app/utils.py
@@ -1,16 +1,17 @@
 import statistics
 from collections import Counter
+import matplotlib.pyplot as plt
+import io
+import base64
 
-def calculate_stats(berries_names, growth_times):
-    """
-    Calculate min, max, mean, median, variance an frequency
-    """
+
+def calculate_stats(berries_names, growth_times, freq=None):
     if not growth_times:
-        raise ValueError("There are no growth times.")
+        raise ValueError("No growth_time data available.")
+    if freq is None:
+        freq = dict(Counter(growth_times))
 
-    freq = dict(Counter(growth_times))
-
-    stats = {
+    return {
         "berries_names": berries_names,
         "min_growth_time": min(growth_times),
         "max_growth_time": max(growth_times),
@@ -19,4 +20,19 @@ def calculate_stats(berries_names, growth_times):
         "variance_growth_time": round(statistics.variance(growth_times), 2) if len(growth_times) > 1 else 0,
         "frequency_growth_time": freq
     }
-    return stats
+
+
+def generate_histogram(growth_times):
+    plt.figure(figsize=(8, 4))
+    plt.hist(growth_times, bins=range(min(growth_times), max(growth_times) + 2), color='#A6CE39', edgecolor='black')
+    plt.title("Histogram of Berry Growth Times")
+    plt.xlabel("Growth Time")
+    plt.ylabel("Frequency")
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close()
+    buf.seek(0)
+    img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    html = f'<img src="data:image/png;base64,{img_base64}"/>'
+    return html
